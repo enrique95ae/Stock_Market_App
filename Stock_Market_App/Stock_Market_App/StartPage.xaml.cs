fd17a977f7d9cf011ca825175799c9447d678722
enrique95ae <enrique95ae@gmail.com>
2019-03-10 00:32:58 -0800

    json fetching and parsing

--- Stock_Market_App/Stock_Market_App/StartPage.xaml.cs
+++ Stock_Market_App/Stock_Market_App/StartPage.xaml.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
 using Xamarin.Forms;
-using StockData;
+using StockDataNamespace;
 
 namespace Stock_Market_App
 {
@@ -14,7 +14,12 @@ namespace Stock_Market_App
     public partial class StartPage : ContentPage
     {
         public string API_Key = "7MW8GOBODP71QU9L";
-        public List<TimeSeriesDaily> days;
+        public List<StockData> days;
+        public List<TimeSeriesDaily> stockList;
+        double min = 999999.99;
+        double max = 000000.00;
+        double foundMin = 0;
+        double foundMax = 0;
 
         public StartPage()
         {
@@ -24,7 +29,7 @@ namespace Stock_Market_App
         async void GetStockButton_Clicked(object sender, EventArgs e)
         {
             //Getting company's acronym entered by the user in order to be added to the api endpoint
-            string userInput = "";
+            string userInput;
             userInput = stockEntry.Text;
 
             //Adding the user input to the base api endpoint and converting into an "Uri" variable.
@@ -33,7 +38,7 @@ namespace Stock_Market_App
 
             //creating a HTTP client variable
             HttpClient client = new HttpClient();
-            TimeSeriesDaily stockData = new TimeSeriesDaily();
+            //TimeSeriesDaily stockData = new TimeSeriesDaily();
 
             //Response holder
             HttpResponseMessage response = await client.GetAsync(stockApiUri);
@@ -42,50 +47,44 @@ namespace Stock_Market_App
             string jsonContent = await response.Content.ReadAsStringAsync();
 
             //Parse into an object accroding to our data model (TimeSeriesDaily)
-            stockData = JsonConvert.DeserializeObject<TimeSeriesDaily>(jsonContent);
+            var stockData = StockData.FromJson(jsonContent);
 
-            //adding the newly received objects into the list.
-            days.Add(stockData);
-
-            //setting the list of received data as the source for the listview
-            historyListView.ItemsSource = days;
+            stockList = stockData.TimeSeriesDaily.Values.ToList();
 
 
-            //PopulatePage();
+            historyListView.ItemsSource = stockList;
+
+            PopulatePage();
+
         }
+
 
         public void PopulatePage()
         {
-            //================================START OF GETTING ABSOLUTE MAX AND MINS ================================
-            double min = 999999.99;
-            double max = 000000.00;
-            double foundMin = 0;
-            double foundMax = 0;
-           
-            
-            for(int i=0; i<days.Count; i++)
+            //================================START OF GETTING ABSOLUTE MAX AND MINS ===============================
+
+            foreach (var stock in stockList)
             {
-
-                Console.WriteLine(days[i].The3Low);
-
-                if(double.Parse(days[i].The3Low) < min)
+                // Console.WriteLine(days[i].TimeSeriesDaily.The2high);
+                if(double.Parse(stock.The3Low) < min)
                 {
-                    foundMin = double.Parse(days[i].The3Low);
+                    foundMin = double.Parse(stock.The3Low);
                 }
 
-                if(double.Parse(days[i].The2High) > max)
+                if(double.Parse(stock.The2High) > max)
                 {
-                    foundMax = double.Parse(days[i].The2High);
+                    foundMax = double.Parse(stock.The2High);
                 }
             }
+
 
             lowestLabel.Text = foundMin.ToString();
             highestLabel.Text = foundMax.ToString();
         }
-
-        //================================ END OF GETTING ABSOLUTE MAX AND MINS ================================
-
-
-
     }
 }
+               
+
+             
+    
+
