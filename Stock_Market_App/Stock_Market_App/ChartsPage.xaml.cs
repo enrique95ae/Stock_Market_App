bb70d856ebb9e192d6b418aac0146943f5df88d8
enrique95ae <enrique95ae@gmail.com>
2019-03-09 17:51:46 -0800

    Initial commit

---
+++ Stock_Market_App/Stock_Market_App/ChartsPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Stock_Market_App
+{
+    public partial class ChartsPage : ContentPage
+    {
+        public ChartsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
